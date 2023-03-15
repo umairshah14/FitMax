6f3a2efb1821c1bddb334abacd27a25ae89416ba
umair shah <umairshah14@hotmail.com>
2023-03-15 20:02:48 +0000

    added new path for workouts page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import { BrowserRouter  as Router,Routes,Route} from "react-router-dom";
 import Home from "./components/Home";
+import WorkoutPage from './components/Youtube/workoutPage'; 
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Router>
       <div>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path='/Workouts' element = {<WorkoutPage/>}/>
         </Routes>
       </div>
     </Router>
