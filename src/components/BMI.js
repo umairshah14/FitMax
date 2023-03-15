5c6e8dd2d63a7612935e4d4d3a8dd4110e9f8b75
Algaba <89382004+Algaba84@users.noreply.github.com>
2023-03-15 18:45:03 +0000

    tailwind, MT and card

--- src/components/BMI.js
+++ src/components/BMI.js
@@ -1,8 +1,19 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Input,
+  Button,
+  Textarea
+} from "@material-tailwind/react";
+
 
 function BMI() {
-    const [bmi, setBmi] = useState();
+   const [bmi, setBmi] = useState();
 
     const [bodyData, setBodyData] = useState({
         age: 30,
@@ -33,7 +44,48 @@ function BMI() {
           });
       }, [bodyData.age, bodyData.height, bodyData.weight]);
 
-      return ( <div>{bmi}</div>);
+      return ( <div>
+        <div className="flex justify-center">
+  <Card className="w-min bg-indigo-800 mt-12 ml-12 p-1" >
+      <CardHeader
+        variant="gradient"
+        className="grid h-8 place-items-center bg-indigo-50 text-indigo-800"
+      > BMI
+      </CardHeader>
+      <CardBody className="flex flex-col gap-4">
+        <Input   variant='static' label="Age" size="lg" />
+        <Input  className="" label="Kg" size="lg" />
+        <Input  className="" label="cm" size="lg" />
+      </CardBody>
+      <CardFooter className="pt-0">
+        <Button  fullWidth className="text-indigo-800 bg-indigo-50">
+          Calculate my BMI
+        </Button>
+      </CardFooter>
+    </Card>
+    
+    </div>
+       {/*} <div className="shadow-lg rounded-lg bg-white p-6">
+          
+           
+           <div className="flex flex-col">
+             <input className="border border-gray-400 p-2 rounded-lg  mb-2" type="text"  />
+             <input className="border border-gray-400 p-2 rounded-lg  mb-2" type="text"  />
+           </div>
+           <div className="grid grid-rows-3 grid-flow-col gap-4">
+  <div className="row-span-3 rounded-md border-2 border-indigo-800">
+    <h2 className="text-lg font-bold mb-2">BMI</h2>
+    <div className="flex flex-col items-end">
+             <input className="mr-5 w-24 border border-gray-400 p-2 rounded-lg  mb-2" type="text"  />
+             <input className="mr-5 w-24 border border-gray-400 p-2 rounded-lg  mb-2" type="text"  />
+           </div>
+  </div>
+  <div class="col-span-3 rounded-md ">02</div>
+  
+</div>
+      </div>*/}
+      
+      </div>);
 
 }
 
