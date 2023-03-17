e68148bd872ca4492f07298ff740b5b4d8494ef9
Adil Seco <adil.seco@outlook.com>
2023-03-17 01:43:31 +0000

    added carousel component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,47 @@
-//import { useEffect, useState } from "react";
-import BMI from "./BMI";
-import DailyCalorie from "./DailyCalorie";
+
+import UncontrolledExample from "./Carousel";
+import Header from "./Header/index";
+
 function Home() {
 
+    // const slides = 
+    // [
+    //     {
+    //         url: "../assets/images/running.jpg"
+    //     },
+    //     {
+    //         url: "./assets/images/running2.jpg"
+    //     },
+    //     {
+    //         url: "./assets/images/old_people.jpg"
+    //     },
+    // ];
+
     return (
-        <div>
-            <div><BMI /></div>
-            <div><DailyCalorie /></div>
-        </div>
+        <>
+            <header />
+            <main>
+                {/* div for carousel */}
+                {/* <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative"> */}
+                    <div> <UncontrolledExample /> </div>
+                {/* </div> */}
+                
+                {/* div for importance of health */}
+                <div>
+                    
+                </div>  
+
+                {/* div for our sponsors */}
+                <div>
+
+                </div>
+
+                {/* div for google ads */}
+                <div>
+
+                </div>
+            </main>    
+        </>
     );
 
 }
