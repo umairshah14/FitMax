b521f108d0c574df5ef7a26df218ff3cf3c19b77
umair shah <umairshah14@hotmail.com>
2023-03-21 19:43:35 +0000

    fitness page styling added

--- src/components/Fitness.js
+++ src/components/Fitness.js
@@ -1,74 +1,96 @@
 import BMI from "./BMI";
 import DailyCalorie from "./DailyCalorie";
-import BodyFat from "./BodyFat"
+import BodyFat from "./BodyFat";
 import IdealWeight from "./IdealWeight";
-import {Button,} from "@material-tailwind/react"
+import Button from "@mui/material/Button"; // importing the Material-UI button component
 import { useEffect, useState } from "react";
-import {Container, Row, Col} from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap";
 
 function Fitness() {
-    const [active, setActive] = useState("BMI");
+  const [searchTerm, setSearchTerm] = useState("BMI"); // state to hold the search term entered by the user
 
-    const [localData, setLocalData] = useState();
+  const buttons = [
+    { label: "BMI", searchTerm: "BMI" },
+    { label: "Daily Calories", searchTerm: "DailyCalories" },
+    { label: "Body Fat", searchTerm: "BodyFat" },
+    { label: "Ideal Weight", searchTerm: "IdealWeight" },
+  ];
+  const [activeIndex, setActiveIndex] = useState(null); // state to hold the index of the currently active button
 
-    const [resultData, setResultData] = useState({
-        bmi: localStorage.getItem("bmiData") ? JSON.parse(localStorage.getItem("bmiData")).bmi : "",
-        health: localStorage.getItem("bmiData") ? JSON.parse(localStorage.getItem("bmiData")).health : ""
-    });
+  const handleClick = (index) => {
+    setSearchTerm(buttons[index].searchTerm); // updating the search term state with the term associated with the clicked button
+    setActiveIndex(index); // updating the active button index state
+  };
 
-    useEffect(() => {
-        if(localData) {
-            setResultData({
-                bmi: localData.bmi,
-                health: localData.health
-            })
-        }
-    }, [localData]);
+  const [localData, setLocalData] = useState();
 
-    const getLocal= () => {
-        const local = JSON.parse(localStorage.getItem("bmiData"));
-        if(local) {
-            setLocalData({
-                bmi: local.bmi,
-                health: local.health
-            });
-        }
-    };
+  const [resultData, setResultData] = useState({
+    bmi: localStorage.getItem("bmiData")
+      ? JSON.parse(localStorage.getItem("bmiData")).bmi
+      : "",
+    health: localStorage.getItem("bmiData")
+      ? JSON.parse(localStorage.getItem("bmiData")).health
+      : "",
+  });
 
-    return (
-        <div>
-            <Button  className="text-indigo-800 bg-indigo-50 m-2" onClick={() => setActive("BMI")}>
-               BMI
-            </Button>
-            <Button  className="text-indigo-800 bg-indigo-50" onClick={() => setActive("DailyCalories")}>
-               Daily Calories
-            </Button>
-            <Button  className="text-indigo-800 bg-indigo-50" onClick={() => setActive("BodyFat")}>
-               Body Fat
-            </Button>
-            <Button  className="text-indigo-800 bg-indigo-50" onClick={() => setActive("IdealWeight")}>
-               Ideal Weight
-            </Button>
-            <Container className="mt-10">
-              <Row >
-              <Col lg={8} className="mb-10">
-                {active === "BMI" && <BMI getLocal={getLocal}/>}
-                {active === "DailyCalories" && <DailyCalorie />}
-                {active === "BodyFat" && <BodyFat />}
-                {active === "IdealWeight" && <IdealWeight />}
-              </Col>
-              <Col lg={4} className=" pt-2 border-2 border-indigo-800 rounded-lg px-8">
-                
-                  <p><span className="font-bold text-xl">Your BMI is: </span>{resultData.bmi}</p>
-                  <p><span className="font-bold text-xl">Your Health is: </span>{resultData.health}</p> 
-               
-              </Col>
-              </Row>  
-            </Container>
-            
-        </div>
-    );
+  useEffect(() => {
+    if (localData) {
+      setResultData({
+        bmi: localData.bmi,
+        health: localData.health,
+      });
+    }
+  }, [localData]);
 
+  const getLocal = () => {
+    const local = JSON.parse(localStorage.getItem("bmiData"));
+    if (local) {
+      setLocalData({
+        bmi: local.bmi,
+        health: local.health,
+      });
+    }
+  };
+
+  return (
+    <div>
+      <Container id="workOutBtns">
+        {buttons.map((button, index) => (
+          <Button
+            key={index}
+            variant={activeIndex === index ? "contained" : "outlined"}
+            onClick={() => handleClick(index)}
+          >
+            {button.label}
+          </Button>
+        ))}
+      </Container>
+
+      <Container className="mt-10">
+        <Row>
+          <Col lg={8} className="mb-10">
+            {searchTerm === "BMI" && <BMI getLocal={getLocal} />}
+            {searchTerm === "DailyCalories" && <DailyCalorie />}
+            {searchTerm === "BodyFat" && <BodyFat />}
+            {searchTerm === "IdealWeight" && <IdealWeight />}
+          </Col>
+          <Col
+            lg={4}
+            className=" pt-2 border-2 border-indigo-800 rounded-lg px-8"
+          >
+            <p>
+              <span className="font-bold text-xl">Your BMI is: </span>
+              {resultData.bmi}
+            </p>
+            <p>
+              <span className="font-bold text-xl">Your Health is: </span>
+              {resultData.health}
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default Fitness;
